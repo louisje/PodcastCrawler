fd7ef07abc6b5c33b9bfc0af249665a4588baed8
louis <louis@9c8da41c-bf06-0410-8773-8f36092d53a2>
2010-11-25 10:18:40 +0000

    File locking mechenism
    git-svn-id: svn+ssh://192.168.0.25/var/svnroot/boss/9x9Services_PHP/PodcastCrawler/trunk@14701 9c8da41c-bf06-0410-8773-8f36092d53a2

--- podcastcrawler.php
+++ podcastcrawler.php
@@ -2,6 +2,15 @@
 	
 	require_once dirname(__FILE__) . "/common.php";
 	
+	/**
+	 * Create lock file
+	 */
+	$sPidFile = $cfgLogDir . '/podcastcrawler.pid';
+	if (file_exists($sPidFile))
+		die("\nERROR: another instance is running!\n\n");
+	if (!file_put_contents($sPidFile, getmypid()))
+		die("\nERROR: fail to create lock file!\n\n");
+	
 	try {
 		$objCrawler = new OneAppleCrawler();
 		$objCrawler -> crawl();
@@ -10,5 +19,12 @@
 		Util :: log ("Exception: " . $e -> getMessage(), MODE_ERROR);
 		Util :: log (" + File: " . $e -> getFile(), MODE_ERROR);
 		Util :: log (" + Line: " . $e -> getLine(), MODE_ERROR);
-		die("\nERROR: " . $e -> getMessage() . "\n\n");
+		echo "\nERROR: " . $e -> getMessage() . "\n\n";
 	}
+	
+	/**
+	 * Remove lock file
+	 */
+	if (!unlink($sPidFile))
+		die("\nWARNING: fail to remove lock file, you may need to remove it manually.\n\n");
+	
