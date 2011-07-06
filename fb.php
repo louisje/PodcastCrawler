4c68cb43c9519d2593d66a9f09e9d1347195e82f
louis <louis@9c8da41c-bf06-0410-8773-8f36092d53a2>
2011-07-06 07:37:02 +0000

    facebook page to rss
    git-svn-id: svn+ssh://192.168.0.25/var/svnroot/boss/9x9Services_PHP/PodcastCrawler/trunk@14717 9c8da41c-bf06-0410-8773-8f36092d53a2

---
+++ fb.php
@@ -0,0 +1,63 @@
+<?php
+	
+	require_once dirname(__FILE__) . "/common.php";
+	
+	define('FB_GRAPH_API_URL', 'http://graph.facebook.com/');
+	
+	if (empty ($_REQUEST['id']))
+		die ("Missing parameters!");
+	
+	$sUrl = constant('FB_GRAPH_API_URL') . $_REQUEST['id'];
+	$objFBProfile = json_decode(Util :: sendHttpRequest($sUrl));
+	//print_r($objFBProfile);
+	if (isset ($objFBProfile -> error))
+		die ($objFBProfile -> error -> type . ": " . $objFBProfile -> error -> message);
+	else if (empty ($objFBProfile))
+		die ("Decoding error!");
+	
+	$sUrl = $sUrl . "/feed";
+	$mFBFeed = json_decode(Util :: sendHttpRequest($sUrl));
+	//print_r($mFBFeed);
+	if (isset ($mFBFeed -> error))
+		die ($mFBFeed -> error -> type . ": " . $mFBFeed -> error -> message);
+	else if (empty ($objFBProfile))
+		die ("Decoding error!");
+	
+	$arrFBVideos = array();
+	foreach ($mFBFeed -> data as $objFBPost) {
+		if (isset($objFBPost -> source) && strpos($objFBPost -> source, '.mp4') != FALSE)
+			$arrFBVideos[] = $objFBPost;
+	}
+	//print_r($arrFBVideos);
+	
+	$objXmlRss = new SimpleXmlElement('<?xml version="1.0" encoding="utf-8"?><rss version="2.0" />');
+	$objXmlChannel = $objXmlRss -> addChild('channel');
+	
+	$objXmlChannel -> addChild('title', $objFBProfile -> name);
+	if (isset($objFBProfile -> link))
+		$objXmlChannel -> addChild('link', $objFBProfile -> link);
+	if (isset($objFBProfile -> picture)) {
+		$objXmlImage = $objXmlChannel -> addChild('image');
+		$objXmlImage -> addChild('url', $objFBProfile -> picture);
+		$objXmlImage -> addChild('title', $objXmlChannel -> title);
+		$objXmlImage -> addChild('link', $objXmlChannel -> link);
+	}
+	//$arrXmlItems = array();
+	foreach ($arrFBVideos as $objFBVideo) {
+		$objXmlItem = $objXmlChannel -> addChild('item');
+		$objXmlItem -> addChild('title', $objFBVideo -> name);
+		if (isset ($objFBVideo -> message))
+			$objXmlItem -> addChild('description', htmlspecialchars($objFBVideo -> message));
+		
+		$objXmlEnclosure = $objXmlItem -> addChild('enclosure');
+		$objXmlEnclosure -> addAttribute('url', $objFBVideo -> source);
+		$objXmlEnclosure -> addAttribute('length', Util :: getRemoteFileSize($objFBVideo -> source));
+		$objXmlEnclosure -> addAttribute('type', 'video/mp4');
+		$objXmlItem -> addChild('duration', $objFBVideo -> properties[0] -> text);
+		
+		$objXmlItem -> addChild('pubDate', date('r', strtotime($objFBVideo -> created_time)));
+		$objXmlItem -> addChild('link', htmlspecialchars($objFBVideo -> link));
+		$objXmlItem -> addChild('guid', $objFBVideo -> object_id);
+	}
+	echo $objXmlRss -> asXML();
+	
